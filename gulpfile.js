7cacab174f73ad45eccb306e1dee8bb77681eef1
pixilz <eoudon@gmail.com>
2019-01-14 19:21:07 -0700

    Updated gulp to create static assets for pages (for non SSR) Also removed old npm package dependencies that were only needed for SSR

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	sass = require('gulp-sass');
-
-gulp.task('task-name', function() {
-	// Stuff here
-});
+	sass = require('gulp-sass'),
+	pug = require('gulp-pug'),
+	data = require('gulp-data'),
+	fs = require('fs'),
+	path = require('path'),
+	merge = require('gulp-merge-json');
 
 gulp.task('scss', function() {
-	var input = '**/stylesheets/**/*.scss',
+	const input = '**/stylesheets/**/*.scss',
 		output = 'public/stylesheets/css';
 
 	return gulp
@@ -18,4 +19,43 @@ gulp.task('scss', function() {
 		.pipe(concat('styles.css'))
 		// Write the resulting CSS in the output folder
 		.pipe(gulp.dest(output));
+});
+
+gulp.task('pug:data', function() {
+	return gulp
+		.src('templateData/**/*.json')
+		.pipe(merge({
+			fileName: 'data.json',
+			edit: (json, file) => {
+				let filename = path.basename(file.path),
+					primaryKey = filename.replace(path.extname(filename), '');
+
+				let data = {};
+
+				data[primaryKey.toUpperCase()] = json;
+
+				return data;
+			}
+		}))
+		.pipe(gulp.dest('/temp'));
+});
+
+gulp.task('pug', gulp.series('pug:data', function() {
+	return gulp
+		.src('views/index.pug')
+		.pipe(data(function() {
+			return JSON.parse(fs.readFileSync('/temp/data.json'));
+		}))
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./'));
+}));
+
+gulp.task('pug:watch', function() {
+	return gulp.watch('views/**/*.pug', gulp.parallel('pug'));
+});
+
+gulp.task('scss:watch', function() {
+	return gulp.watch('**/stylesheets/**/*.scss', gulp.parallel('scss'));
 });
